60f2f6db64b241c64d6a500055c06d1ca02046ad
sajid ali <sajid.ali@folio3.com>
2024-07-04 21:47:06 +0500

    added GDPR routes in api.php

--- web/routes/api.php
+++ web/routes/api.php
@@ -21,6 +21,63 @@ Route::get('/', function () {
     return "Hello API";
 });
 
+define('CLIENT_SECRET', env('SHOPIFY_API_SECRET'));
+function verify_webhook($data, $hmac_header)
+{
+    $calculated_hmac = base64_encode(hash_hmac('sha256', $data, CLIENT_SECRET, true));
+    return hash_equals($calculated_hmac, $hmac_header);
+}
+
+// Customer Data Erasure Webhook
+Route::post('/customers/redact', function () {
+    $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+
+    $data = file_get_contents('php://input');
+    $verified = verify_webhook($data, $hmac_header);
+    error_log('Webhook verified: ' . var_export($verified, true));
+    if ($verified) {
+        # Process webhook payload
+        # ...
+        http_response_code(200);
+    } else {
+        response()->json('', 401)->send();
+    }
+});
+
+// Customer Data request Webhook
+Route::post('/customers/data_request', function () {
+
+    $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+
+    $data = file_get_contents('php://input');
+    $verified = verify_webhook($data, $hmac_header);
+    error_log('Webhook verified: ' . var_export($verified, true));
+    if ($verified) {
+        # Process webhook payload
+        # ...
+        http_response_code(200);
+    } else {
+        response()->json('', 401)->send();
+    }
+});
+
+// Customer Data request Webhook
+Route::post('/redact/redact', function () {
+
+    $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+
+    $data = file_get_contents('php://input');
+    $verified = verify_webhook($data, $hmac_header);
+    error_log('Webhook verified: ' . var_export($verified, true));
+    if ($verified) {
+        # Process webhook payload
+        # ...
+        http_response_code(200);
+    } else {
+        response()->json('', 401)->send();
+    }
+});
+
 
 // Routes for shopify
 Route::group(['middleware' => 'shopify.auth',  'prefix' => 'shopify'], function () {
